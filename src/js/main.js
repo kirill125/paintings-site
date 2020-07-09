1f171b00350ab53a5d5d5052fadb711d39b7cc08
Kirill Skvortsov <truemancheg@gmail.com>
2020-07-09 22:29:07 +0300

    filter elements by tabs

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ import mask from "./modules/mask";
 import checkTextInputs from "./modules/checkTextInputs";
 import showMoreStyles from "./modules/showMoreStyles";
 import calc from "./modules/calc";
+import filter from "./modules/filter";
 
 window.addEventListener("DOMContentLoaded", () => {
     "use strict";
@@ -17,4 +18,5 @@ window.addEventListener("DOMContentLoaded", () => {
     checkTextInputs("[name='message']");
     showMoreStyles(".button-styles", "#styles .row");
     calc("#size", "#material", "#options", ".promocode", ".calc-price");
+    filter();
 });
